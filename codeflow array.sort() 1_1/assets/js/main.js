b84a8b1b9f0bc3ca3a79b4fe51631e9111c169dc
emilysupercode <sheil.emily.sc@gmail.com>
2022-11-03 11:12:22 +0100

    exercises complete

---
+++ codeflow array.sort() 1_1/assets/js/main.js
@@ -0,0 +1,22 @@
+console.log("test")
+
+let languages = [
+    "JavaScript",
+    "Python",
+    "Java",
+    "Ruby",
+    "PHP",
+    "C++",
+    "CSS",
+    "C#",
+    "Go",
+    "C",
+    "TypeScript",
+    "Swift"
+];
+
+function sortierung() {
+    languages.sort();
+}
+sortierung();
+console.log(languages)
---
+++ js array 1_3/assets/js/main.js
@@ -0,0 +1,11 @@
+console.log("test")
+
+var numArray1 = [36, 24, 22, 3, 2, 98, 88, 99, 10, 54, 68, 70];
+
+console.log(numArray1)
+
+const numArray2 = numArray1.sort((a, b) => a - b)
+console.log(numArray2)
+
+const numArray3 = numArray2.reverse()
+console.log(numArray3)
